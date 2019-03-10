9052cd9d7ddfc7674bdab33f94a76cbfa6b059ed
DingQiangLiu <dingqiangliu@gmail.com>
2019-03-10 10:49:11 +0800

    Resolved the issue that HPL/SQL can not select Date and Timestamp type values into variables from RDBMS

--- vplsql/src/test/testDatatype.sql
+++ vplsql/src/test/testDatatype.sql
@@ -10,7 +10,7 @@ as
   n numeric(18,6);
   s varchar; 
   d date; 
-  dt timestamp; 
+  dt timestamp(18, 3); 
 begin
   i := 0;
   for j IN 1..10 loop
@@ -18,7 +18,28 @@ begin
   end loop;
   print('sigma from 1 to 10 = ' || i);
 
-  -- Note: hplsql-0.3.17 只支持从数据库取值的类型CHAR/VARCHAR/INTEGER/BIGINT/SMALLINT/TINYINT/DECIMAL/NUMERIC/FLOAT/DOUBLE, see: org.apache.hive.hplsql.Var.setValue @ 261 line.
+  i = null;
+  i = 1550636626;
+  f = 1550636625.678;
+  n = 1550636625.678;
+  s = '1550636625.678';
+  d = '2019-02-20 12:23:45';
+  dt = '2019-02-20 12:23:45.678';
+  print('Set variable:');
+  print('    int = ' || i);
+  print('    float = ' || f);
+  print('    numeric = ' || n);
+  print('    varchar = ' || s);
+  print('    date = ' || d);
+  print('    datetime = ' || dt);
+
+  i = null;
+  i = null;
+  f = null;
+  n = null;
+  s = null;
+  d = null;
+  dt = null;
   exec 'select
     extract(epoch from ''2019-02-20 12:23:45.678''::datetime)::int
     , extract(epoch from ''2019-02-20 12:23:45.678''::datetime)::float
